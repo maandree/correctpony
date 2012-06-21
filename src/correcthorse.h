8b80b573438f4ea87ce94786798d9968dfe9ed09
Mattias Andrée <maandree@operamail.com>
2012-06-21 20:58:11 +0200

    m

--- src/correcthorse.h
+++ src/correcthorse.h
@@ -1,3 +1,13 @@
+/**
+ * correcthorse - a passphrase generator inspired by http://xkcd.com/936/
+ * Copyright (c) 2012  Mattias Andrée (maandree@kth.se)
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ */
 #ifndef CORRECTHORSE_H
 #define CORRECTHORSE_H
 
--- src/wlist.h
+++ src/wlist.h
@@ -1,7 +1,15 @@
+/**
+ * correcthorse - a passphrase generator inspired by http://xkcd.com/936/
+ * Copyright (c) 2012  Mattias Andrée (maandree@kth.se)
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ */
 #ifndef WLIST_H
 #define WLIST_H
-
-#include "correcthorse.h"
 
 #include <stdlib.h>
 
