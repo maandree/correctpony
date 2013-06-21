5a7f6ebfe13e028b3f779b5ad49da3d0381ad277
Mattias Andrée <maandree@operamail.com>
2013-06-21 19:43:30 +0200

    m doc
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ src/Program.java
@@ -0,0 +1,41 @@
+/**
+ * correctpony — a passphrase generator inspired by xkcd 936
+ * 
+ * Copyright © 2012, 2013  Mattias Andrée (maandree@member.fsf.org)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/**
+ * Mane class
+ * 
+ * Mattias Andrée, <a href="mailto:maandree@member.fsf.org">maandree@member.fsf.org</a>
+ */
+public class Program
+{
+    /**
+     * Mane method
+     * 
+     * @param  args  Command line arguments, excluding exec
+     * 
+     * @throws  IOException  On I/O error
+     */
+    public static void main(final String... args) throws IOException
+    {
+	
+    }
+    
+}
+
