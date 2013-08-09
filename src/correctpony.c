752ef1a7bd92815fa11fb80a5c6d240db580e982
Mattias Andrée <maandree@operamail.com>
2013-08-09 04:07:39 +0200

    stronger license = better license
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/correctpony.c
+++ src/correctpony.c
@@ -2,11 +2,18 @@
  * correctpony - a passphrase generator inspired by http://xkcd.com/936/
  * Copyright (C) 2012, 2013  Mattias Andrée (maandree@member.fsf.org)
  * 
- * This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "correctpony.h"
 
@@ -108,7 +115,19 @@ static void print_version(char* argv0)
 {
   printf("%s Version: " VERSION "\n", argv0);
   printf("Copyright (C) 2012, 2013  Mattias Andrée (maandree@member.fsf.org)\n");
-  printf("License: WTFPL 2.0 http://sam.zoy.org/wtfpl/COPYING\n");
+  printf("\n");
+  printf("This program is free software: you can redistribute it and/or modify\n");
+  printf("it under the terms of the GNU Affero General Public License as published by\n");
+  printf("the Free Software Foundation, either version 3 of the License, or\n");
+  printf("(at your option) any later version.\n");
+  printf("\n");
+  printf("This program is distributed in the hope that it will be useful,\n");
+  printf("but WITHOUT ANY WARRANTY; without even the implied warranty of\n");
+  printf("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n");
+  printf("GNU Affero General Public License for more details.\n");
+  printf("\n");
+  printf("You should have received a copy of the GNU Affero General Public License\n");
+  printf("along with this program.  If not, see <http://www.gnu.org/licenses/>.\n");
 }
 
 int main(int argc, char** argv)
--- src/correctpony.h
+++ src/correctpony.h
@@ -2,11 +2,18 @@
  * correctpony - a passphrase generator inspired by http://xkcd.com/936/
  * Copyright (C) 2012, 2013  Mattias Andrée (maandree@member.fsf.org)
  * 
- * This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef CORRECTPONY_H
 #define CORRECTPONY_H
